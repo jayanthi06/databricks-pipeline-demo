dacac6cefcbace10d7342a39b2fdbd6061f1f722
jayanthikotrakona <97394496+jayanthi06@users.noreply.github.com>
2025-04-11 15:04:42 -0400

    Add files via upload

---
+++ sample_notebook.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Sample Notebook for DevOps Pipeline
+
+# COMMAND ----------
+print("Hello from Databricks notebook!")
+
+# COMMAND ----------
+# Example computation
+result = sum([i for i in range(10)])
+print(f"Sum of 0-9 is: {result}")
